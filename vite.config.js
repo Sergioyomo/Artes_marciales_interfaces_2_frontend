2e513f3d6ea40e3b2e9a1752cc63581a08f29a94
Sergioyomo <xsgomsan834@ieshnosmachado.org>
2025-03-01 18:41:56 +0100

    actualizar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Asegura rutas relativas
+  build: {
+    outDir: "dist", // Output folder
+  }
 })
